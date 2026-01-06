3387c70993718dc83cf7b3d32669ffb3be275ae2
LeyvinoBezerra <leyvinobezerra.develop@gmail.com>
2026-01-06 16:33:10 -0300

    Finalizando a Tarde

--- Arrays/array_splice.js
+++ Arrays/array_splice.js
@@ -1,8 +1,17 @@
 const months = [ "Janeiro", "Fevereiro", "Maio", "Segunda-Feira", "Terça-Feira" ];
 
+// splice(3)
+// Remove todos os elementos a partir do índice 3 (inclusive).
+// Retorna os elementos removidos em um novo array.
+// Aqui, remove ["Segunda-Feira", "Terça-Feira"] e guarda em daysOfWeek.
 const daysOfWeek = months.splice(3);
+
+// splice(2, 0, "Março", "Abril")
+// No índice 2, remove 0 elementos e insere "Março" e "Abril".
+// Isso insere os novos meses sem remover nada.
+// O retorno é um array vazio, pois nada foi removido.
 const emptyArray = months.splice(2, 0, "Março", "Abril");
 
-console.log(months);
-console.log(daysOfWeek);
-console.log(emptyArray);
+console.log(months);      // ["Janeiro", "Fevereiro", "Março", "Abril", "Maio"]
+console.log(daysOfWeek);  // ["Segunda-Feira", "Terça-Feira"]
+console.log(emptyArray);  // []
