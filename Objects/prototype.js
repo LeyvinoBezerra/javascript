9f4e2994955048fb437e31f682676ed0fab45ace
LeyvinoBezerra <leyvinobezerra.develop@gmail.com>
2026-01-11 15:00:25 -0300

    estudando 11/01/26

--- Objects/prototype.js
+++ Objects/prototype.js
@@ -1,15 +1,37 @@
+// Criando um objeto chamado "vehicle" com duas propriedades:
+// - speed: 0.0
+// - drive: uma função que imprime "drive!" no console
 const vehicle = {
     speed: 0.0,
     drive: function() { console.log("drive!"); }
-}
+};
 
+// Lendo a propriedade "speed" do objeto vehicle.
+// Resultado: 0.0
 console.log(vehicle.speed);
+
+// Chamando o método "drive" do objeto vehicle.
+// Resultado: imprime "drive!" no console
 vehicle.drive();
 
+// Criando um novo objeto chamado "car" que herda de "vehicle"
+// através de Object.create(vehicle). Ou seja, "car" terá acesso
+// às propriedades e métodos definidos em "vehicle".
 const car = Object.create(vehicle);
+
+// Adicionando uma nova propriedade "color" ao objeto car.
+// Agora car tem "color: blue".
 car.color = "blue";
+
+// Alterando a propriedade "speed" especificamente no objeto car.
+// Isso sobrescreve o valor herdado de vehicle (0.0) para 100.0.
 car.speed = 100.0;
 
+// Exibindo a propriedade "speed" do objeto car.
+// Resultado: 100.0 (valor próprio de car, não o herdado de vehicle)
 console.log(car.speed);
+
+// Chamando o método "drive" no objeto car.
+// Como car não tem seu próprio método "drive", ele usa o herdado de vehicle.
+// Resultado: imprime "drive!" no console
 car.drive();
-console.log(car.color);
--- Objects/prototype2.js
+++ Objects/prototype2.js
@@ -1,26 +1,50 @@
+// Criando um objeto chamado "vehicle" com duas propriedades:
+// - speed: 0.0
+// - drive: função que imprime "drive!" no console
 const vehicle = {
     speed: 0.0,
     drive: function() { console.log("drive!"); }
-}
+};
 
+// Criando um novo objeto "car" que herda de "vehicle".
+// Assim, car terá acesso às propriedades e métodos de vehicle.
 const car = Object.create(vehicle);
+
+// Adicionando uma nova propriedade própria ao objeto car: "color" = "blue"
 car.color = "blue";
 
+// Exibindo os nomes das propriedades próprias de "vehicle".
+// Resultado: [ 'speed', 'drive' ]
 console.log(Object.getOwnPropertyNames(vehicle));
+
+// Exibindo os nomes das propriedades próprias de "car".
+// Resultado: [ 'color' ]
 console.log(Object.getOwnPropertyNames(car));
 
+// Usando um loop for...in para listar todas as propriedades acessíveis em "car",
+// incluindo as herdadas de "vehicle".
 for (const p in car) {
     console.log("-> " + p);
+    // Resultado: 
+    // -> color   (própria de car)
+    // -> speed   (herdada de vehicle)
+    // -> drive   (herdada de vehicle)
 }
 
+// Verificando se a propriedade "color" existe e tem valor truthy.
+// Como car.color = "blue", imprime:
 if (car.color) {
     console.log("The car color is " + car.color);
 }
 
+// Verificando se "color" não é undefined.
+// Também imprime "The car color is blue".
 if (car.color !== undefined) {
     console.log("The car color is " + car.color);
 }
 
+// Verificando se a chave "color" existe dentro de car (mesmo que herdada).
+// Como "color" é própria de car, imprime novamente:
 if ("color" in car) {
     console.log("The car color is " + car.color);
 }
