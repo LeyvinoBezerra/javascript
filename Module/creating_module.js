bbdf4ddfad100ae325765b04924c53797ef58c69
LeyvinoBezerra <leyvinobezerra.develop@gmail.com>
2026-01-19 14:05:33 -0300

    começando a semana já finalizando este curso

--- Module/creating_module.js
+++ Module/creating_module.js
@@ -1,9 +1,19 @@
+// Importando exportes nomeados do arquivo math.js
+// "PI" é importado com o nome alternativo "piNumber"
+// "sum" é importado como "sumOp"
+// "double" é importado como "doubleOp"
 import { PI as piNumber, sum as sumOp, double as doubleOp } from './math.js';
+
+// Importando o export default do arquivo math.js
+// Como "multiply" foi exportado como default, não precisa de chaves
 import multiply from './math.js';
 
-//import * as m from './math.js'
+// Também seria possível importar tudo de uma vez em um objeto "m"
+// (essa linha está comentada)
+// import * as m from './math.js'
 
-console.log(piNumber);
-console.log(sumOp(1, 3));
-console.log(doubleOp(4));
-console.log(multiply(2, 5));
+// Imprimindo os valores e resultados das funções
+console.log(piNumber);       // imprime 3.1416
+console.log(sumOp(1, 3));    // imprime 4 (soma de 1 + 3)
+console.log(doubleOp(4));    // imprime 8 (dobro de 4)
+console.log(multiply(2, 5)); // imprime 10 (multiplicação 2 * 5)
--- Module/math.js
+++ Module/math.js
@@ -1,16 +1,28 @@
+// Declarando uma constante
 const PI = 3.1416;
 
+// Função que soma dois números
 function sum(a, b) {
     return a + b;
 }
 
+// Função que dobra um número
 function double(a) {
     return a * 2;
 }
 
+// Função que multiplica dois números
 function multiply(a, b) {
     return a * b;
 }
 
+// Exportando múltiplos itens de forma "nomeada"
+// Aqui estamos exportando PI, sum e double
+// Para importar, usamos chaves: import { PI, sum, double } from './arquivo.js';
 export { PI, sum, double };
+
+// Exportando um item como "default"
+// Cada módulo só pode ter UM export default
+// Aqui, multiply é o export padrão
+// Para importar, não usamos chaves: import multiply from './arquivo.js';
 export default multiply;
